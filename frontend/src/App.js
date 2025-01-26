37eb0f6b752ed4d4979f4b40f429623367db1e4f
Thitipat Charonekitpasuk <owen2546@gmail.com>
2025-01-26 22:28:17 +0700

    operalink

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Homepage from './component/Homepage/Homepage';
 import Login from './component/Login/Login';
 import RegisterChoice from './component/RegisterChoice/RegisterChoice';
 import RegisterCustomer from './component/Register/RegisterCustomer';
+import OperaRequri from './component/OperaRequri/Opera';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path="/RegisterChoice" element={<RegisterChoice />} />
         <Route path="/login" element={<Login />} /> 
         <Route path="/customer-register" element={<RegisterCustomer/>} />
-
+        <Route path="/OperaRequri" element={<OperaRequri/>} />
       </Routes>
     </BrowserRouter>
   );
